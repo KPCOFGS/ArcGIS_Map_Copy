92f9b63f3a88b6d3759466f2e5a01384df1478e1
Shi Sheng <a13375572211@gmail.com>
2024-03-11 19:22:54 -0400

    Add files via upload

---
+++ copy_map.py
@@ -0,0 +1,18 @@
+from arcgis.gis import GIS
+
+# Source GIS, URL stays the same
+src_gis = GIS("https://www.arcgis.com", "SOURCE_ACCOUNT", "SOURCE_PASSWORD")
+
+# Destination GIS, URL stays the same
+dest_gis = GIS("https://www.arcgis.com", "DISTINATION_ACCOUNT", "DISTINATION_PASSWORD")
+
+# ID of the map you want to copy
+map_id = "MAP_ID"
+
+# Search for the map in the source GIS
+src_item = src_gis.content.get(map_id)
+
+# Copy the map to the destination GIS
+dest_item = dest_gis.content.clone_items(items=[src_item])
+
+print("Map copied successfully.")
